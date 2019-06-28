0c9164906afebe0d9f377e5d453465d6fa9416ce
mateoradic16 <mateoradic16@gmail.com>
2019-06-28 21:18:53 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@ sleep 2
 
 cd /home/container
 
-mkdir steam && cd steam
+mkdir /home/container/steam
+cd steam
 curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
-
 cd /home/container
 
 # Update Unturned Server
